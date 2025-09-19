b24b9a1c25c6ad913a89ae1b82adac6fee3bd16b
SoyWadyRD <andreslopez261316@gmail.com>
2025-09-19 14:33:55 -0400

    Eliminadas opciones deprecated de mongoose connect

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,14 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('🟢 MongoDB conectado');
   } catch (error) {
     console.error('Error al conectar a MongoDB:', error.message);
-    process.exit(1); // Para que el servidor se detenga si falla la conexión
+    process.exit(1);
   }
 };
 
